2cc80a88f69ca1a3fb0ffd3fe826183c4d53eed6
s_dbz <s_dbz@Ratsata-Note.home>
2016-11-14 00:41:09 -0300

    Asignatura Curso funcionando

--- src/trabajo/Asignaturas/jfAsignarCurso.java
+++ src/trabajo/Asignaturas/jfAsignarCurso.java
@@ -1,9 +1,49 @@
 package trabajo.Asignaturas;
 
-public class jfAsignarCurso extends javax.swing.JFrame {
+import clases.Conexion;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.sql.ResultSet;
+import java.util.Calendar;
+import javax.swing.JOptionPane;
 
+public class jfAsignarCurso extends javax.swing.JFrame {
+    private Conexion BD = new Conexion();
+    private String msj;
+    private ResultSet rs;
+    private String id_curso;
+    private String id_asignatura;
+    
     public jfAsignarCurso() {
+        Image icon = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/imagenes/book_edit.png"));
+        setIconImage(icon);
         initComponents();
+        this.setLocationRelativeTo(null); //CENTRAR EN LA PANTALLA
+        
+        
+        //Llenar ComboBoxs
+        try{
+            
+            BD.crearConexion();
+            //PRIMER COMBOBOX
+            String sql = "SELECT nombre FROM curso";
+            rs = BD.ejecutarSQLSelect(sql);
+            while(rs.next()){
+                cmbCurso.addItem(rs.getString("nombre"));
+                
+            }
+            //SEGUNDO COMBOBOX
+            sql = "SELECT nombre FROM asignatura";
+            rs = BD.ejecutarSQLSelect(sql);
+            while(rs.next()){
+                cmbAsignatura.addItem(rs.getString("nombre"));
+                
+            }
+            BD.cerrarConexion();
+        }catch (Exception e){
+            msj = "Error, hubo un problema.";
+            JOptionPane.showMessageDialog(null,msj,"Error",JOptionPane.ERROR_MESSAGE);    
+        }
     }
 
     @SuppressWarnings("unchecked")
@@ -11,25 +51,29 @@ public class jfAsignarCurso extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox<>();
-        jLabel2 = new javax.swing.JLabel();
-        jComboBox2 = new javax.swing.JComboBox<>();
-        jButton1 = new javax.swing.JButton();
+        lblTitulo = new javax.swing.JLabel();
         btn_volver = new javax.swing.JButton();
+        btnAceptar = new javax.swing.JButton();
+        cmbCurso = new javax.swing.JComboBox<>();
+        lblCurso = new javax.swing.JLabel();
+        cmbAsignatura = new javax.swing.JComboBox<>();
+        lblAsignatura = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(400, 250));
+        setMinimumSize(new java.awt.Dimension(400, 250));
+        setPreferredSize(new java.awt.Dimension(400, 250));
+        setResizable(false);
+        getContentPane().setLayout(null);
 
-        jLabel1.setText("Seleccione asignatura:");
+        jPanel1.setMaximumSize(new java.awt.Dimension(400, 250));
+        jPanel1.setMinimumSize(new java.awt.Dimension(400, 250));
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Biologia" }));
+        lblTitulo.setFont(new java.awt.Font("Tempus Sans ITC", 1, 18)); // NOI18N
+        lblTitulo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/book_edit.png"))); // NOI18N
+        lblTitulo.setText("Asignar Curso");
 
-        jLabel2.setText("Seleccione curso:");
-
-        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Primero Basico A" }));
-
-        jButton1.setText("Asignar");
-
+        btn_volver.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/backward.png"))); // NOI18N
         btn_volver.setText("Volver");
         btn_volver.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -37,54 +81,66 @@ public class jfAsignarCurso extends javax.swing.JFrame {
             }
         });
 
+        btnAceptar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/accept.png"))); // NOI18N
+        btnAceptar.setText("Asignar");
+        btnAceptar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAceptarActionPerformed(evt);
+            }
+        });
+
+        lblCurso.setText("Seleccione curso:");
+
+        lblAsignatura.setText("Seleccione asignatura:");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(30, 30, 30)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(jLabel1)
-                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(btn_volver)
-                        .addGap(47, 47, 47)))
-                .addGap(60, 60, 60)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2)
-                    .addComponent(jButton1))
-                .addContainerGap(27, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(lblTitulo))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(35, 35, 35)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblAsignatura)
+                            .addComponent(cmbAsignatura, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(61, 61, 61)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(cmbCurso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(lblCurso))))
+                .addContainerGap(113, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btn_volver)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnAceptar)
+                .addGap(18, 18, 18))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
+                .addComponent(lblTitulo)
+                .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel2))
+                    .addComponent(lblAsignatura)
+                    .addComponent(lblCurso))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                    .addComponent(cmbAsignatura, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cmbCurso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 64, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
+                    .addComponent(btnAceptar)
                     .addComponent(btn_volver))
-                .addContainerGap(33, Short.MAX_VALUE))
+                .addGap(39, 39, 39))
         );
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+        getContentPane().add(jPanel1);
+        jPanel1.setBounds(0, 0, 400, 250);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -93,8 +149,35 @@ public class jfAsignarCurso extends javax.swing.JFrame {
         jfAsignaturas asig = new jfAsignaturas();
         this.dispose();
         asig.setVisible(true);
-
     }//GEN-LAST:event_btn_volverActionPerformed
+
+    private void btnAceptarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAceptarActionPerformed
+        String coma = "','";
+        try {
+            BD.crearConexion();
+            String sql = "SELECT a.id_asignatura, c.id_curso FROM asignatura a, curso c WHERE a.nombre = '"+ cmbAsignatura.getSelectedItem() +"' AND c.nombre = '"+ cmbCurso.getSelectedItem() +"'";
+            rs = BD.ejecutarSQLSelect(sql);
+            while (rs.next()) {
+                id_curso = rs.getString("c.id_curso");
+                id_asignatura = rs.getString("a.id_asignatura");
+            }
+            //Fecha
+            Calendar fecha = Calendar.getInstance();
+            int anno = fecha.get(Calendar.YEAR);
+            sql = "INSERT INTO asignatura_curso(id_curso,id_asignatura,anno) VALUES ('" + id_curso + coma + id_asignatura + coma + anno + "')";
+            if (BD.ejecutarSQL(sql)) {
+                msj = "Se registro la asociacion correctamente";
+                JOptionPane.showMessageDialog(null, msj, "Exito", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                msj = "Error, Ya existe esta asociacion";
+                JOptionPane.showMessageDialog(null, msj, "Error", JOptionPane.ERROR_MESSAGE);
+            }
+            BD.cerrarConexion();
+        } catch (Exception e) {
+            msj = "Error, hubo un problema.";
+            JOptionPane.showMessageDialog(null, msj, "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }//GEN-LAST:event_btnAceptarActionPerformed
 
     public static void main(String args[]) {
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -126,12 +209,13 @@ public class jfAsignarCurso extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAceptar;
     private javax.swing.JButton btn_volver;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JComboBox<String> jComboBox1;
-    private javax.swing.JComboBox<String> jComboBox2;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JComboBox<String> cmbAsignatura;
+    private javax.swing.JComboBox<String> cmbCurso;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lblAsignatura;
+    private javax.swing.JLabel lblCurso;
+    private javax.swing.JLabel lblTitulo;
     // End of variables declaration//GEN-END:variables
 }
