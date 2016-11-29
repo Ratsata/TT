6135688b2c754e81684517492e274c1f81a55fe4
s_dbz <s_dbz@Ratsata-Note.home>
2016-11-29 04:52:48 -0300

    finish?

--- src/trabajo/Notas/jfNotas.java
+++ src/trabajo/Notas/jfNotas.java
@@ -97,6 +97,11 @@ public class jfNotas extends javax.swing.JFrame {
 
         cmbNota.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "n/a", "70", "69", "68", "67", "66", "65", "64", "63", "62", "61", "60", "59", "58", "57", "56", "55", "54", "53", "52", "51", "50", "49", "48", "47", "46", "45", "44", "43", "42", "41", "40", "39", "38", "37", "36", "36", "35", "34", "33", "32", "31", "30", "29", "28", "27", "26", "25", "24", "23", "22", "21", "20", "19", "18", "17", "16", "15", "14", "13", "12", "11", "10", "09", "08", "07", "06", "05", "04", "03", "02", "01", "00" }));
 
+        jCurso.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                jCursoFocusGained(evt);
+            }
+        });
         jCurso.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyTyped(java.awt.event.KeyEvent evt) {
                 jCursoKeyTyped(evt);
@@ -344,6 +349,10 @@ public class jfNotas extends javax.swing.JFrame {
             deshabilitarBotones();
         }
     }//GEN-LAST:event_btnBuscarActionPerformed
+
+    private void jCursoFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jCursoFocusGained
+        jCurso.selectAll();
+    }//GEN-LAST:event_jCursoFocusGained
 
     public static void main(String args[]) {
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
